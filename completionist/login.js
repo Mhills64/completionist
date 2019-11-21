d62eecf7fe5e176355851e5465cc2b3792b1138b
Mhills64 <54864754+Mhills64@users.noreply.github.com>
2019-11-21 13:49:10 -0500

    Created mock-login box

---
+++ completionist/login.js
@@ -0,0 +1,6 @@
+$("#login").click(function(e) {
+
+    let pword = $("#password").value();
+    let uname = $("#username").value();
+    window.location = "file:///C:\\Users\\retro\\PhpstormProjects\\completionist1\\completionist\\main.html";
+});
